6c9a0ef588db0b778ee16ba35099bf5265d7133d
Puyodead1 <puyodead@protonmail.com>
2024-10-11 21:52:16 -0400

    Initialize

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_j3toplteue.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_j3toplteue-user \
+    omni_j3toplteue-userdebug \
+    omni_j3toplteue-eng
